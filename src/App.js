ab1b9663a63e5e69bb3dba1afa3f24298f7e32bd
Javier Minchala <javierminchala@Javiers-MacBook-Air.local>
2019-06-17 11:14:03 -0400

    Set up CSS and app structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import LoggedInMasterView from './components/layout/LoggedInMasterView';
 import './scss/main.scss';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import LoggedInMasterView from './components/layout/LoggedInMasterView';
+import LoggedOutView from './components/layout/LoggedOutView';
+import PostPreview from './components/posts/PostPreview';
 
 export default class App extends Component {
-  constructor (props) {
-    super(props);
-  }
+  // constructor (props) {
+  //   super(props);
+  // }
 
   render() {
     return (
-      <div>
-        <LoggedInMasterView />
-      </div>
+      <Router>
+
+        <Switch>
+          <Route exact path = "/post/preview" component = { PostPreview } />
+          <Route exact path = "/" component = { LoggedOutView } />
+        </Switch>
+
+      </Router>
     )
   }
 }
--- src/components/layout/LoggedOutView.js
+++ src/components/layout/LoggedOutView.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 export default class LoggedOutView extends Component {
     render() {
         return (
-            <div>
-                
+            <div className = "logged-out-layout">
+                <div className = "logged-out-inner">
+                     <h1>Get Started</h1>
+                    <button className = "login-btn">Sign In</button>
+                    <button className = "login-btn register-btn">Register</button>
+
+                    <a href = "#" className = "forgot-password">Forgot your password?</a>
+                </div>
             </div>
         )
     }
