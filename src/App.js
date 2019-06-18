6c7828f44795d4f60adb3df93bdc9667c6650290
Javier Minchala <javierminchala@Javiers-MacBook-Air.local>
2019-06-18 11:35:28 -0400

    Updated login page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default class App extends Component {
     return (
       <Router>
         <Switch>
+          <Route path = "/" component = { LoggedOutView } />
           <Route component = { LoggedInMasterView } />
         </Switch>
       </Router>
