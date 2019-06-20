93c0a160d84202966c0b329b87465b5001cd6db5
Tyler <tptorres5@gmail.com>
2019-06-20 13:40:46 -0400

    Got rid of report on user activity

--- src/components/Comments/CommentList.js
+++ src/components/Comments/CommentList.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import SingleComment from './SingleComment';
 
 export default class CommentList extends Component {
-    render() {
-        return (
-            <div className = "comment-list">
-                { this.props.commentList.map((c, i ) => {
-                    return (
-
-                        <SingleComment key = {i} data = { c } />
-
-                    )
-                }) }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='comment-list'>
+        {this.props.commentList.map((c, i) => {
+          return <SingleComment key={i} data={c} />;
+        })}
+      </div>
+    );
+  }
 }
--- src/components/Comments/SingleComment.js
+++ src/components/Comments/SingleComment.js
@@ -4,10 +4,8 @@ export default class SingleComment extends Component {
   render() {
     return (
       <div className='comment-item'>
-        x<div className='text'>{this.props.data.text}</div>
-        <div className='controls-bar'>
-          <button className='report'>Flag</button>
-        </div>
+        {this.props.data.text}
+        <button>View</button>
       </div>
     );
   }
